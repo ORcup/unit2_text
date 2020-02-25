cf486074e0c4ba0d8d4343df9c3ae13a53fd5491
try <try@try.com>
2020-02-25 12:06:55 +0800

    [change]

--- unit2/count_word.py
+++ unit2/count_word.py
@@ -1,7 +1,7 @@
 import read.md as f
 def count_word(fn,):
+    word_number=0
     for line in f:    
-       word_number = 0
        word = line.split()
        word_number += len(word)
     return word_number
